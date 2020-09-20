f364d43838336e84893fa61bc365ae67f4cfed05
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-09-20 01:54:44 -0400

    Split page load get and step post requests

--- src/vote_request.py
+++ src/vote_request.py
@@ -8,13 +8,21 @@ paths = {
     '0': session.getData('base_url')
 }
 
-def requestStep(step):
-    response = requests.get(paths.get(step, ''))
-    if ():
-        print('ERROR: No path for step ' + step)
-        sys.exit()
+def pageLoad():
+    baseUrl = session.getData('base_url')
+    response = requests.get(baseUrl)
     if (response.status_code != 200):
-        print('ERROR: status_code' + response.status_code)
+        print('ERROR: status_code ' + str(response.status_code))
+        print('Requested URL: ' + baseUrl)
+        sys.exit()
+    return response
+
+def executeStep(step, data=[], headers=[]):
+    if (paths.get(step, '') == ''):
+        print('ERROR: path does not exist for step ' + step)
+    response = requests.post(paths.get(step), data, headers)
+    if (response.status_code != 200):
+        print('ERROR: status_code ' + str(response.status_code))
         print('Requested URL: ' + paths.get(step))
         sys.exit()
     return response
