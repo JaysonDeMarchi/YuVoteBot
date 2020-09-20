87d077e3554cac34593d9277c57b6c5356a3cfbc
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-09-20 01:55:27 -0400

    Add base vote executable

---
+++ src/vote.py
@@ -0,0 +1,28 @@
+from . import \
+    parser, \
+    validate, \
+    vote_request, \
+    vote_response
+import time
+
+steps = [
+    '0'
+]
+
+def execute(voteCount = 1):
+    validate.baseUrl()
+    print('Executing {:d} vote(s) . . .'.format(voteCount))
+    for currentVote in range(0, voteCount):
+        response = vote_request.pageLoad()
+        for step in steps:
+            index = step
+            startTime = int(time.time() * 1000)
+            question = parser.parseResponseToQuestion(response)
+            requestData = vote_response.buildData(
+                parser.getFormData(response),
+                index,
+                question,
+                startTime
+            )
+            headers = vote_response.buildHeaders(response)
+            response = vote_request.executeStep(index, requestData, headers)
